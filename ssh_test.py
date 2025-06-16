c22dfa2d5202a874d3f7203f9c5070cc8f650eb4
esther mensah <edmworkmail@gmail.com>
2025-06-16 15:28:41 +0200

    change of library from netmiko to paramiko

--- ssh_test.py
+++ ssh_test.py
@@ -1,27 +1,65 @@
 
-"""
-This script tests SSH connectivity to a Linux device using Netmiko. 
--It connects to the device
-- Executes the command "show version"
-- Prints the output
-- Disconnects from the device
-"""
-
-from netmiko import ConnectHandler
-
-device = {
-    'device_type': 'linux',
-    'ip': os.getenv("DEVICE_IP"),
-    'username': os.getenv("DEVICE_USER"),
-    'password': os.getenv("DEVICE_PASS"),
-}
+# # source venv/Scripts/activate
 
 
-connection = ConnectHandler(**device)
-output = connection.send_command("whoami")
-print(output)
-connection.disconnect()
+import os
+import paramiko
+
+ip = os.getenv("DEVICE_IP")
+username = os.getenv("DEVICE_USER")
+password = os.getenv("DEVICE_PASS")
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+ssh.connect(ip, username=username, password=password)
+
+# stdin, stdout, stderr = ssh.exec_command('cmd /c echo Hello from Windows')
+stdin, stdout, stderr = ssh.exec_command('echo Hello from Windows')
+
+print("Command Output:\n", stdout.read().decode())
+
+
+ssh.close()
 
 
 
+
+
+
+
+
+
+# import os
+# from netmiko import ConnectHandler
+
+# """
+# This script tests SSH connectivity to a Linux device using Netmiko. 
+# -It connects to the device
+# - Executes the command "show version"
+# - Prints the output
+# - Disconnects from the device
+# """
+
+
+
+# device = {
+#     'device_type': 'generic_termserver',
+#     'ip': os.getenv("DEVICE_IP"),
+#     'username': os.getenv("DEVICE_USER"),
+#     'password': os.getenv("DEVICE_PASS"),
+#     'session_log': 'debug_output.txt',
+#     'global_delay_factor': 2,
+# }
+
+
+
+# connection = ConnectHandler(**device)
+# # output = connection.send_command("whoami", read_timeout=20)
+# # output = connection.send_command("echo Hello from SSH", read_timeout=20)
+# output = connection.send_command("cmd /c echo Hello from Windows")
+# # output = connection.send_command("cmd /c hostname")
+
+# print("Command Output:\n",output)
+# connection.disconnect()
 
